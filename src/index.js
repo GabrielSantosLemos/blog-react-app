43390569ffec5fb3ff19cc21a5a7614e3d3956b0
gabrielsantoslemos <gabriel.lemos1001@gmail.com>
2021-07-18 16:01:28 -0300

    Correções de erros Material ui, construindo página de criação de post.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 
+// remove alerta de erro Warning: findDOMNode is deprecated in StrictMode
+import { ThemeProvider, unstable_createMuiStrictModeTheme } from '@material-ui/core/styles';
+
+const theme = unstable_createMuiStrictModeTheme();
+
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
