7c127222ea922215bcb28bcaa044cab887348798
gabriel.lemos1001@gmail.com <gabriel.lemos1001@gmail.com>
2021-07-21 16:57:56 -0300

    Correçôes.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Routes from './routes'
 
-
-import Auth from './components/Auth';
-import store from './store';
-import PrivateRoutes from './routes/PrivateRoutes';
+import App from './App';
 
 // remove alerta de erro Warning: findDOMNode is deprecated in StrictMode
 import { ThemeProvider, unstable_createMuiStrictModeTheme } from '@material-ui/core/styles';
@@ -17,14 +13,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Provider store={store}>
-      <BrowserRouter>
-        <Auth>
-          <Routes />
-        </Auth>
-      </BrowserRouter>
-    </Provider>
-      
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
